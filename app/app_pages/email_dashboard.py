0d72d9489c8786f3e68111e14ecce5c491745211
therese-debacker <ther.debacker@gmail.com>
2025-01-23 15:22:29 +0100

    colors in streamlit and final dashboard

--- app/app_pages/email_dashboard.py
+++ app/app_pages/email_dashboard.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-from sqlalchemy import create_engine
 import sys
 import datetime
 from pathlib import Path
@@ -14,30 +13,25 @@ from api_client import APIClient
 def dataframe_creation():
     # Fetch data from external endpoint
     api_client = APIClient()
-    tracking_data = api_client.get_events()
-    return pd.DataFrame(tracking_data)
+    tracking_data_events = api_client.get_events()
+    tracking_data_campaigns = api_client.get_campaigns()
+    return pd.DataFrame(tracking_data_events), pd.DataFrame(tracking_data_campaigns)
 
-events_df = dataframe_creation()
+events_df, campaigns_df = dataframe_creation()
 
-all_event_types = ["open", "click", "submitted", "downloaded_attachement","reported"]
-event_counts = events_df.groupby(["campaign_id", "event_type"]).size().unstack(fill_value=0)
-
-for event in all_event_types:
-    if event not in event_counts.columns:
-        event_counts[event] = 0
-
-# Establish a connection with the database
-engine = create_engine("sqlite:///./phishing_campaigns.db")
+def create_final_dataframe(events_df,campaigns_df):
+    all_event_types = ["open", "click", "submitted", "downloaded_attachement","reported"]
+    event_counts = events_df.groupby(["campaign_id", "event_type"]).size().unstack(fill_value=0)
+    for event in all_event_types:
+        if event not in event_counts.columns:
+            event_counts[event] = 0
  
-# Read the sqlite table
-campaigns_df = pd.read_sql_table(
-    "campaigns",
-    con=engine
-)
- 
-# Create final dataFrame
-merged_df = pd.merge(campaigns_df, event_counts, left_on="id", right_on="campaign_id", how="left")
-merged_df = merged_df.fillna(0)
+    # Create final dataFrame
+    df = pd.merge(campaigns_df, event_counts, left_on="id", right_on="campaign_id", how="left")
+    df = df.fillna(0)
+    return df
+
+merged_df = create_final_dataframe(events_df, campaigns_df)
 
 # Pie charts
 def pie_chart(data):
@@ -53,6 +47,7 @@ def pie_chart(data):
     st.pyplot(fig, use_container_width=True)
     plt.clf()
 
+# Percentage calculations
 def calculate_click_rate(
     campaign_clicks, campaign_opens, campaign_sent, campaign_submitted, campaign_reports, campaign_download
 ):
@@ -63,12 +58,15 @@ def calculate_click_rate(
     downloads = round(campaign_download / campaign_sent * 100, 2)
     return open_rate, click_rate, data_submitted, reports, downloads
 
+# Creation of the dashboard on Streamlit
 def graphs(df):
     # Time data
     df['created_at'] = pd.to_datetime(df['created_at'])
-    df['month'] = df['created_at'].dt.strftime('%B')
+    df['month_word'] = df['created_at'].dt.strftime('%B')
     df['year'] = df['created_at'].dt.year
     df['day'] = df['created_at'].dt.day
+    df['month'] = df['created_at'].dt.month
+    df['date'] = pd.to_datetime(df[['day', 'month', 'year']])
 
     # Sidebar
     with st.sidebar:
@@ -80,20 +78,19 @@ def graphs(df):
             df = df
         else:
             df = df[df["year"] == years]
-            unique_months = df["month"].unique()
+            unique_months = df["month_word"].unique()
             selected_month = st.multiselect("Select a month", options=unique_months)
             if not selected_month:
                 df = df
             else:
-                df = df[df["month"].isin(selected_month)]
+                df = df[df["month_word"].isin(selected_month)]
         if on:
             unique_campaign = df["name"].unique()
             selected_campaign = st.selectbox("Select a campaign", unique_campaign)
             df = df[df["name"] == selected_campaign]
             
 
-    # COMMENT AVOIR LES SENT
-    # emails_sent_df = df["sent"].sum() # HOW ????
+    # Campaigns data
     emails_sent = df['target_count'].sum()
     emails_open = df['open'].sum()
     emails_click = df['click'].sum()
@@ -133,78 +130,50 @@ def graphs(df):
     
     # Bar charts
     if on:
+        def addlabels(values):
+            for i, value in enumerate(values):
+                plt.text(i, value + 0.01*max(values), str(value), ha='center', fontsize=8)  # Adjust 0.05 to change the distance
+
         st.markdown("### Campaign details")
-        categories = ["Sent", "Opens", "Click", "PDF downloads", "Data submitted", "Reported"]
+        plt.rcParams.update({'font.size': 8})
+        categories = ["Sent", "Opened", "Clicked", "PDF downloads", "Data submitted", "Reported"]
         values = [emails_sent, emails_open, emails_click, emails_pdf, emails_submitted, emails_reported]
         fig, ax = plt.subplots()
-        ax.bar(categories, values, color=["#5C2D91", "#7d57a7", "#DE2A56"])
-        ax.set_ylabel("Number of Emails")
+        ax.bar(categories, values, color=["#5C2D91", "#754db8", "#AD96C8", "#DE2A56", "#6F142B", "#C0C1C4"])
+        plt.xticks(rotation=45)
+        ax.set_ylabel("How many people", fontsize=10)
+        addlabels(values)
+
         st.pyplot(fig)
     else:
-        st.markdown("### Overview of the lasts campaigns")
         if years == None:
+            st.markdown("### Overview of the lasts campaigns")
             current_date = datetime.datetime.now()  
             one_year_ago = current_date - pd.DateOffset(months=12) 
             df = df[df['created_at'] >= one_year_ago]
         else:
+            st.markdown(f"### Overview of the campaigns in {years}")
             df = df[df["year"] == years]
         
+        df_last_10 = df.tail(10)
         fig, ax = plt.subplots(figsize=(10, 6)) 
-        ax.plot(df['created_at'], df['click'], label="Clicks", color="#7d57a7", marker='o')
-        ax.plot(df['created_at'], df['open'], label="Opens", color="#5C2D91", marker='o')
-        ax.plot(df['created_at'], df['reported'], label="Reports", color="#DE2A56", marker='o')
-        ax.plot(df['created_at'], df['submitted'], label="Submitted", color="#28A745", marker='o')
-        ax.plot(df['created_at'], df['downloaded_attachement'], label="Downloads", color="#28A745", marker='o')
-        ax.set_xlabel("Date")
+        ax.plot(df_last_10['date'], df_last_10['open'], label="Opens", color="#5C2D91", marker='o')
+        ax.plot(df_last_10['date'], df_last_10['click'], label="Clicks", color="#AD96C8", marker='o')
+        ax.plot(df_last_10['date'], df_last_10['submitted'], label="Data submitted", color="#DE2A56", marker='o')
+        ax.plot(df_last_10['date'], df_last_10['downloaded_attachement'], label="PDF downloads", color="#6F142B", marker='o')
+        ax.plot(df_last_10['date'], df_last_10['reported'], label="Reports", color="#C0C1C4", marker='o')
         ax.set_ylabel("Number of events")
-        ax.set_title("Evolution over time")
         ax.legend()
         st.pyplot(fig)
 
     plt.clf()
 
-# #         # RAJOUTER reports & data submitted
-# #         st.markdown("### Overview per business units")
-# #         # business_units = ["Proximus", "Proximus Billing Service", "Proximus Ada"]
-# #         # percentage_clicking = [14, 17, 7]
-# #         # plt.bar(business_units, percentage_clicking, color="#7d57a7")
-# #         # plt.xlabel("Business units")
-# #         # plt.ylabel("Percentage of clicking")
-# #         # st.pyplot(plt)
-# #         business_units = ["Proximus", "Proximus Billing Service", "Proximus Ada"]
-# #         percentage_clicking = [14, 17, 7]  # Pourcentage de clics
-# #         percentage_submitted = [9, 12, 5]  # Pourcentage de soumissions
-# #         percentage_reports = [3, 4, 1]    # Pourcentage de signalements
-
-# #         # Création du graphique
-# #         fig, ax = plt.subplots()
-
-# #         # Tracer les barres pour chaque catégorie
-# #         bar_width = 0.25
-# #         index = range(len(business_units))
-
-# #         # Barres pour les clics, soumissions et signalements
-# #         ax.bar(index, percentage_clicking, bar_width, label="Click rate", color="#7d57a7")
-# #         ax.bar([i + bar_width for i in index], percentage_submitted, bar_width, label="Submitted rate", color="#5C2D91")
-# #         ax.bar([i + bar_width * 2 for i in index], percentage_reports, bar_width, label="Report rate", color="#DE2A56")
-
-# #         # Ajouter des labels et un titre
-# #         ax.set_xlabel("Business units")
-# #         ax.set_ylabel("Percentage (%)")
-# #         # ax.set_title("Comparison of Clicks, Submitted, and Reports by Business Unit")
-# #         ax.set_xticks([i + bar_width for i in index])  # Positionner les étiquettes au centre des groupes de barres
-# #         ax.set_xticklabels(business_units)
-# #         ax.legend()
-
-# #         # Affichage du graphique avec Streamlit
-# #         st.pyplot(fig)
-
-    # dataframe view
+    # Dataframe view
     if on:
         st.markdown(f"### Detailed data view - {selected_campaign}")
     else:
         st.markdown("### Detailed data view - last 20 campaigns")
-    short_df = df.drop(['status', 'month', 'year', 'day'], axis=1)
+    short_df = df.drop(['status', 'month', 'year', 'day', 'month_word', 'date', 'id'], axis=1)
     short_df.sort_values(by='created_at', ascending = False, inplace = True) 
     st.dataframe(short_df.head(20))
 
